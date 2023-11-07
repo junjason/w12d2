1dcf3f153fddb39a0c4f17747fcac1c773d66573
Jason Jun <junjason@umich.edu>
2023-11-07 16:33:31 -0500

    done with initial setup

--- practice-for-ch-react-authenticate-me-long-practice/frontend/src/store/index.js
+++ practice-for-ch-react-authenticate-me-long-practice/frontend/src/store/index.js
@@ -1,24 +1,20 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import session from './session'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
 const rootReducer = combineReducers({
-  session
+
 });
 
 let enhancer;
 
 if (process.env.NODE_ENV === 'production') {
-  enhancer = applyMiddleware(thunk);
+    enhancer = applyMiddleware(thunk);
 } else {
-  const logger = require('redux-logger').default;
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+    const logger = require('redux-logger').default;
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
-const configureStore = (preloadedState) => {
-  return createStore(rootReducer, preloadedState, enhancer);
+export const configureStore = (preloadedState = {}) => {
+    return createStore(rootReducer, preloadedState, enhancer);
 };
-
-export default configureStore;
